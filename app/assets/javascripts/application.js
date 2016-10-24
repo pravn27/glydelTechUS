341f2e819441f1f6aace0557e6e523f9b6bcd58d
jay <jay.jp.prakash@gmail.com>
2016-10-24 22:20:50 +0530

    intermediate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require angular.min
-//= require angular-route.min
 //= require app
 //= require_tree .
--- app/controllers/vehicle_controller.rb
+++ app/controllers/vehicle_controller.rb
@@ -1,11 +1,11 @@
 class VehicleController < ApplicationController
 
 	def create 
-		
+		@vehicle=current_user.companies.vehicles.create(company_data)
 	end
 
 	def edit
-
+		
 	end
 
 	def destroy
@@ -16,4 +16,8 @@ class VehicleController < ApplicationController
 
 	end
 
+	private
+	def vehicle_data
+		params.permit(:vehicle_type,:vehicle_number,:vin_number,:make,:model,:year,:fuel_capacity,:fuel_type)
+	end
 end
