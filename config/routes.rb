075093f5a2de45c0765d175d97688baaacf6ddc8
Karthi <karthi30.it@gmail.com>
2016-10-24 18:34:44 +0530

    route fixes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'vehicles/new' => "vehicle#add"
   get 'vehicles/details' => "vehicle#detail"
   get 'issues' => "vehicle#issues"
-  get 'schedule' => "schedule#index"
+  get 'schedule' => "schedules#index"
 end
