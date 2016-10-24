a283a001a15df1f8df108ea3104fd18437aa0b5e
Subash <honeyasm@gmail.com>
2016-10-24 17:18:42 +0530

    pages added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
+  get 'dashboard' =>'home#dashboard'
   get 'vehicles' => "vehicle#index"
   get 'vehicles/new' => "vehicle#add"
   get 'vehicles/details' => "vehicle#detail"
+  get 'issues' => "vehicle#issues"
 end
