91e54bc4bb32d66752f5a0b61238ab7d63bfc6ae
jay <jay.jp.prakash@gmail.com>
2016-10-24 18:28:56 +0530

    angular

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -1,5 +1,4 @@
-class VehicleController < ApplicationController
-
+class TrackController < ApplicationController
 	def create 
 		
 	end
@@ -15,5 +14,4 @@ class VehicleController < ApplicationController
 	def update
 
 	end
-
 end
---
+++ app/models/vehicle.rb
@@ -0,0 +1,4 @@
+class Vehicle
+  include Mongoid::Document
+  field :first_name, type: String
+end
