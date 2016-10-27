4720f16db2e771720f0b81bb5777c3bc6ef9dd66
Karthi <karthi30.it@gmail.com>
2016-10-27 19:29:23 +0530

    fixes

--- app/assets/javascripts/angular/shared/directive.js
+++ app/assets/javascripts/angular/shared/directive.js
@@ -22,6 +22,35 @@ app.directive('autoComplete',function(){
 		}
 	}
 })
+
+app.directive('vehicleAutoComplete',function(){
+	return {
+		restrict :"A",
+		scope:{
+			onSelected:'&'
+		},
+		link:function(scope,elem,attrs){
+			console.log(elem)
+			elem.autocomplete({
+				source: function(req,resp){
+					$.getJSON("/schedules/vehicles.json?search="+req.term,function(res){
+						var data=[]
+						$.each(res.data,function(i,d){
+							data.push({"label":d.vehicle_number,"value":d.vehicle_number,"data":d})
+						})
+						resp(data);
+					});
+				},
+				select:function(a,b){
+					scope.$apply(function(){
+						scope.onSelected({$selected:b.item.data})
+					})
+				}
+			})
+		}
+	}
+})
+
 app.directive('datepicker', function() {
     return {
         restrict: 'A',
@@ -30,6 +59,7 @@ app.directive('datepicker', function() {
             $(function(){
                 element.datepicker({
                     dateFormat:'mm/dd/yy',
+                    minDate: 'today',
                     onSelect:function (date) {
                         scope.$apply(function () {
                             ngModelCtrl.$setViewValue(date);
